bcad4bf3d0b3114546296474a31c6feece1b3e58
Markus Leitz <MLeitz@boptics.de>
2016-10-22 11:00:01 +0200

    add two more files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,93 @@
-
+#include <windows.h>
 #include <iostream>
-int main(int argc, char **argv)
+#include "RobotMsg.pb.h" 
+#include "MQTTClient.h"
+
+double func(double invalue)
 {
-	std::cout << "Hello ProtoMQTT \n";
+	return 2*invalue;
+}
+
+
+int setupMQTT(MQTTClient &roboClient)
+{
+	//MQTTClient roboClient;
+	MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
+	MQTTClient_willOptions wopts = MQTTClient_willOptions_initializer;
+	std::string  macadress=  "roboClient";//netadapter.getMACAddress();
+
+	//"tcp://192.168.178.24:1884"
+	int rc = MQTTClient_create(&roboClient, "tcp://iot.eclipse.org:1883", macadress.c_str(), MQTTCLIENT_PERSISTENCE_DEFAULT, NULL);
+
+	opts.keepAliveInterval = 20;
+	opts.cleansession = 1;
+	opts.will = &wopts;
+	opts.will->message = "4711 disconnected";
+	opts.will->qos = 1;
+	opts.will->retained = 0;
+	opts.will->topicName = "Robo/Disconnect";
+
+
+	try
+	{
+		rc = MQTTClient_connect(roboClient, &opts);
+
+		if(rc != MQTTCLIENT_SUCCESS)  std::cout << " error \n";
+	}
+	catch(...)
+	{
+		rc = -1;
+	}
+
+	return  0;
+}
+
+int main(int argc, char** argv)
+{
+	int *ptr = nullptr;
+	auto f= 1000/30.0;
+	std::cout << "Hello ProtoMQTT " << f << " " << func(f) << "  \n";
+
+	RobotMsg msg;
+    std::cout << "byte size is " <<  msg.ByteSize() << "\n";
+	msg.set_timestamp(3423435);
+	msg.set_robotstate(RobotMsg_RobotStates_Unkown);
+	msg.set_devicename(std::string("Kraftwerk"));
+	std::cout << "byte size is " <<  msg.ByteSize() << "\n";
+	
+	
+	MQTTClient roboClient;
+	int rc = setupMQTT(roboClient);
+
+	if(NULL != roboClient)
+	{
+		int i=0;
+
+		do 
+		{
+			msg.set_timestamp(i++);
+			MQTTClient_deliveryToken dt;
+			MQTTClient_message pubmsg = MQTTClient_message_initializer;
+			pubmsg.payload = (void*)(msg.SerializeAsString()).c_str();
+			pubmsg.payloadlen =(int)(msg.SerializeAsString()).size();
+			
+
+			pubmsg.retained = 0;
+			std::string topic = "Robo/Info";
+			int rc = MQTTClient_publish(roboClient, topic.c_str(), pubmsg.payloadlen, pubmsg.payload, pubmsg.qos, pubmsg.retained, &dt);
+
+			if (pubmsg.qos > 0)
+			{
+				rc = MQTTClient_waitForCompletion(roboClient, dt, 5000L);
+			}
+
+			Sleep(500);
+
+		} while (1);
+		
+
+	}
+
+	
 	return 0;
 }
