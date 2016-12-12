d02cce7c4daacab29372284914e5f1c95f851977
vlovo <MLeitz@boptis.de>
2016-12-12 09:22:33 +0100

    sync with ::three

--- main.cpp
+++ main.cpp
@@ -1,89 +1,300 @@
 #include <windows.h>
 #include <iostream>
-#include "RobotMsg.pb.h" 
+
+#include "RobotMsg.pb.h"
+
+extern "C"
+{
+
 #include "MQTTClient.h"
 
- 
-int setupMQTT(MQTTClient &roboClient)
+};
+
+
+
+#include "boost/date_time/posix_time/posix_time.hpp"
+
+using namespace boost::posix_time;
+
+void demoPosixTime()
 {
-	MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
-	MQTTClient_willOptions wopts = MQTTClient_willOptions_initializer;
-	std::string  macadress=  "e3::34::43::a2";
 
-	 
-	int rc = MQTTClient_create(&roboClient, "tcp://iot.eclipse.org:1883", macadress.c_str(), MQTTCLIENT_PERSISTENCE_DEFAULT, NULL);
+    ptime t0 = microsec_clock::local_time();
 
-	opts.keepAliveInterval = 20;
-	opts.cleansession = 1;
-	opts.will = &wopts;
-	opts.will->message = "4711 disconnected";
-	opts.will->qos = 1;
-	opts.will->retained = 0;
-	opts.will->topicName = "Robo/Disconnect";
+    std::string  timeString  = to_simple_string(t0);
 
+    std::string  timeIsoString = to_iso_string(t0);
 
-	try
-	{
-		int rc = MQTTClient_connect(roboClient, &opts);
+    ptime t1 = time_from_string(timeString);
 
-		if(rc != MQTTCLIENT_SUCCESS)  std::cout << " error \n";
-	}
-	catch(...)
-	{
-		 
-	}
+    ptime t2 = from_iso_string(timeIsoString);
 
-	return  0;
+    if( t0 == t1 && t1 == t2 )  std::cout << "t0 ,t1 and t2 are equal"  << "\n";
+
+    ptime t3 =  from_iso_string("20161120T170143.558219");
+
+    if(t3 > t1 )
+    {
+        std::cout << " t3 is a new message \n";
+    }
+    else
+    {
+        std::cout << "t3 is a message from the past \n";
+    }
+
+    return ;
 }
 
-	
-	int main(int argc, char** argv)
-	{
-		 
-		std::cout << "Hello ProtoMQTT  \n";
-	
-		RobotMsg msg;
-	    std::cout << "byte size is " <<  msg.ByteSize() << "\n";
-		std::cout << "size is "  << sizeof(msg) << "\n";
-	
-	 
-	msg.set_robotstate(RobotMsg_RobotStates_Unkown);
-	msg.set_devicename(std::string("Kraftwerk"));
-	std::cout << "byte size is " <<  msg.ByteSize() << "\n";
-	
-	
-	MQTTClient roboClient;
-	int rc = setupMQTT(roboClient);
+class RobotCtrl
+{
+public:
 
-	if(NULL != roboClient)
-	{
-		int i=0;
 
-		do 
-		{
-			 
-			MQTTClient_deliveryToken dt;
-			MQTTClient_message pubmsg = MQTTClient_message_initializer;
-			pubmsg.payload = (void*)(msg.SerializeAsString()).c_str();
-			pubmsg.payloadlen =(int)(msg.SerializeAsString()).size();
-			
+    RobotCtrl(std::string robotName)
+    {
+        mRobotMessage.set_robotstate(RobotMsg_RobotStates_Connected);
+        mRobotMessage.set_devicename(robotName);
+        std::string name = mRobotMessage.devicename();
 
-			pubmsg.retained = 0;
-			std::string topic = "Robo/Info";
-			int rc = MQTTClient_publish(roboClient, topic.c_str(), pubmsg.payloadlen, pubmsg.payload, pubmsg.qos, pubmsg.retained, &dt);
+    }
 
-			if (pubmsg.qos > 0)
-			{
-				rc = MQTTClient_waitForCompletion(roboClient, dt, 5000L);
-			}
+    void setClient(MQTTClient &client)
+    {
+        mMQTTClient = client;
+    }
 
-			Sleep(500);
+    ~RobotCtrl() {};
 
-		} while (1);
-		
+    int move(std::vector<double> position)
+    {
+        int ret = 0;
 
-	}
+        if(position.size() >0)
+        {
+            std::cout << "move to " << position[0]  << " " << position[1] << "\n";
 
-	
-	return 0;
+            mRobotMessage.mutable_position()->Clear();
+            mRobotMessage.mutable_position()->Reserve(position.size());
+
+            memcpy(mRobotMessage.mutable_position()->mutable_data(),
+                   position.data(),
+                   position.size()*sizeof(mRobotMessage.position(0)));
+
+            std::cout <<  "1" <<  mRobotMessage.position(0) << "  2 "  << mRobotMessage.position(1) << "  3 " << mRobotMessage.position(2) << " \n";
+
+        }
+
+        return(ret);
+
+    }
+
+    int publishMessage( )
+    {
+        ptime t0 = microsec_clock::local_time();
+        std::string  timeString  = to_simple_string(t0);
+
+        mRobotMessage.set_timestamp(timeString);
+
+        MQTTClient_deliveryToken dt;
+        MQTTClient_message pubmsg = MQTTClient_message_initializer;
+        pubmsg.payload = (void*)(mRobotMessage.SerializeAsString()).c_str();
+        pubmsg.payloadlen =(int)(mRobotMessage.SerializeAsString()).size();
+        pubmsg.retained = 0;
+
+        std::string topic = "Robo/data";
+        int rc=0;
+
+        if(nullptr != mMQTTClient)
+        {
+
+
+            rc = MQTTClient_publish(mMQTTClient,
+                                    topic.c_str(),
+                                    pubmsg.payloadlen,
+                                    pubmsg.payload,
+                                    pubmsg.qos,
+                                    pubmsg.retained,
+                                    &dt);
+
+            if (pubmsg.qos > 0)
+            {
+                rc = MQTTClient_waitForCompletion(mMQTTClient, dt, 5000L);
+            }
+        }
+        else
+        {
+            rc = -1;
+        }
+
+        return (rc);
+    }
+
+
+
+
+
+    static int onMessageArrived(void* context, char* topic , int tlen, MQTTClient_message *msg)
+    {
+
+        RobotCtrl *caller = (RobotCtrl*) context;
+
+        std::string _topic(topic);
+        RobotMsg _msg;
+#ifndef REAL
+        _msg.add_position(1);
+        _msg.add_position(2);
+        _msg.add_position(3);
+
+
+#else
+        _msg.ParseFromArray(msg->payload,msg->payloadlen);
+#endif
+
+        std::vector<double> p(_msg.position_size(),0);
+
+        memcpy(p.data(),
+               _msg.mutable_position()->mutable_data(),
+               _msg.position_size()*sizeof(double));
+
+        caller->move(p);
+
+
+        return(1);
+    }
+
+
+
+
+private:
+    RobotMsg mRobotMessage;
+    MQTTClient mMQTTClient;
+    std::vector<double> mPosition;
+};
+
+
+
+
+
+int setupMQTT(MQTTClient &mqttClient,RobotCtrl &robo)
+{
+
+    std::string  clientID=  "RoboClient";
+
+    int rc = MQTTClient_create(&mqttClient, "tcp://127.0.0.1:1883", clientID.c_str(), MQTTCLIENT_PERSISTENCE_DEFAULT, NULL);
+
+    int rc2 = MQTTClient_setCallbacks(mqttClient, &robo,0, RobotCtrl::onMessageArrived ,0);
+
+
+    MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_willOptions wopts = MQTTClient_willOptions_initializer;
+
+    opts.keepAliveInterval = 20;
+    opts.cleansession = 1;
+    opts.connectTimeout = 2;
+    opts.will = &wopts;
+    opts.will->message = "R2D2 disconnected";
+    opts.will->qos = 1;
+    opts.will->retained = 0;
+    opts.will->topicName = "Robo/disconnect";
+
+    rc = MQTTClient_connect(mqttClient, &opts);
+
+    if(rc != MQTTCLIENT_SUCCESS)
+    {
+        std::cout << " error \n";
+
+    }
+    else
+    {
+        robo.setClient(mqttClient);
+    }
+
+    return  rc;
+}
+
+
+int main(int argc, char** argv)
+{
+
+    demoPosixTime();
+
+
+    std::cout << "Hello ProtoMQTT  \n";
+
+    RobotMsg msg;
+
+    std::cout << "msg byte size is " <<  msg.ByteSize() << "\n";
+    std::cout << "msg size is "  << sizeof(msg) << "\n";
+
+
+    ptime t0 = microsec_clock::local_time();
+    std::string  timeString  = to_simple_string(t0);
+    std::cout << "timeString is " <<timeString.size() <<"\n";
+
+    msg.set_timestamp(timeString);
+    std::cout << " msg byte size is " <<  msg.ByteSize() << "\n";
+
+
+
+
+
+
+    RobotCtrl robotControler("Kraftwerk");
+
+    MQTTClient mqttClient;
+
+    std::string  clientID=  "RoboClient";
+
+    int rc = MQTTClient_create(&mqttClient, "tcp://127.0.0.1:1883", clientID.c_str(), MQTTCLIENT_PERSISTENCE_DEFAULT, NULL);
+    rc = MQTTClient_setCallbacks(mqttClient, &robotControler,0, RobotCtrl::onMessageArrived ,0);
+
+
+    MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_willOptions wopts = MQTTClient_willOptions_initializer;
+
+    opts.keepAliveInterval = 20;
+    opts.cleansession = 1;
+    opts.connectTimeout = 2;
+    opts.will = &wopts;
+    opts.will->message = "R2D2 disconnected";
+    opts.will->qos = 1;
+    opts.will->retained = 0;
+    opts.will->topicName = "Robo/disconnect";
+
+    rc = MQTTClient_connect(mqttClient, &opts);
+
+    if(rc != MQTTCLIENT_SUCCESS)
+    {
+        std::cout << " error \n";
+
+    }
+    else
+    {
+        robotControler.setClient(mqttClient);
+
+    }
+
+
+    int ec = MQTTClient_subscribe(mqttClient,"Robo/Input",0);
+
+    int i=0;
+
+    do
+    {
+
+
+
+        Sleep(500);
+
+
+        robotControler.publishMessage();
+
+    }
+    while (1);
+
+
+
+
+
+    return 0;
 }
